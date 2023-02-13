38f2330adc08020b5cc89e2b5d9f9426d45e64bb
Jack <jw05taylor@gmail.com>
2023-02-13 10:34:27 -0500

    Fixed resolution and pygame.display

--- tetris.py
+++ tetris.py
@@ -2,12 +2,12 @@ import pygame, sys
 
 pygame.init()
 pygame.display.set_caption("Tetris")
-window = pygame.display.set_mode((1000,600))
+window = pygame.display.set_mode((600, 1000))
 running = True
 
 while running:
     for event in pygame.event.get():
         if event == pygame.QUIT:
-            running= False
+            running = False
 
-    pygame.Display.update()
+    pygame.display.update()
